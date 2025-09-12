cf18fe4d668463774b08e64e8381f7f078b2ad48
Sushmita-Hubli <hublisushmita7@gmail.com>
2025-09-12 15:43:13 -0400

    basics of Typescript
    noEmitOnError, global compilation of typescript to javascript , few basic commands for compilation of typescript into js.

---
+++ Lesson1/src/main.ts
@@ -0,0 +1,20 @@
+let username="Sushmita Hubli";
+console.log(username);
+
+
+// let a=6;
+// // let b='2';
+// let b=2;
+// let c=4;
+// console.log(a/b);
+
+//we can also write the abopve variables in the following way:
+
+let a:number=6;
+
+// let b:string='2';
+
+let b:number=2;
+let c:number=4;
+
+console.log(b/a);
